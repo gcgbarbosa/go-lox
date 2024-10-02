0db7cba1b34b5c91d7eb99c0ef71912d677e8745
gcgbarbosa <gcgbarbosa@gmail.com>
2024-10-01 23:31:05 -0300

    fix(pkg/lox): reorder Token struct fields for alignment

--- pkg/lox/token.go
+++ pkg/lox/token.go
@@ -2,7 +2,17 @@ package lox
 
 type TokenType int
 
+// more info on maligned structs:
+// https://medium.com/@sebassegros/golang-dealing-with-maligned-structs-9b77bacf4b97
+type Token struct {
+	Literal interface{}
+	Lexeme  string
+	Line    int
+	Type    TokenType
+}
+
 const (
+	// single-character tokens
 	LEFT_PAREN TokenType = iota // iota starts at 0 and increments for each constant
 	RIGHT_PAREN
 	LEFT_BRACE
@@ -10,13 +20,43 @@ const (
 	COMMMA
 	DOT
 	MINUS
-)
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
 
-// more info on maligned structs:
-// https://medium.com/@sebassegros/golang-dealing-with-maligned-structs-9b77bacf4b97
-type Token struct {
-	Literal interface{}
-	Lexeme  string
-	Line    int
-	Type_   TokenType
-}
+	// one or two character tokens
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+
+	// literals
+	IDENTIFIER
+	STRING
+	NUMBER
+
+	// keywords
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
+	EOF
+)
