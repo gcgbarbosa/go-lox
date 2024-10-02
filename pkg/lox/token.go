ce674d483dd6205a9f4ad883a7e52c964c23d06e
gcgbarbosa <gcgbarbosa@gmail.com>
2024-10-01 23:48:27 -0300

    feat: finish token

--- pkg/lox/token.go
+++ pkg/lox/token.go
@@ -1,5 +1,7 @@
 package lox
 
+import "fmt"
+
 type TokenType int
 
 // more info on maligned structs:
@@ -11,13 +13,28 @@ type Token struct {
 	Type    TokenType
 }
 
+func ToString(t Token) string {
+	var literalStr string
+
+	// Check if t.Literal is a string or something else
+	if t.Literal == nil {
+		literalStr = "nil"
+	} else if literal, ok := t.Literal.(string); ok {
+		literalStr = literal
+	} else {
+		literalStr = fmt.Sprintf("%v", t.Literal)
+	}
+
+	return t.Type.String() + " " + t.Lexeme + " " + literalStr
+}
+
 const (
 	// single-character tokens
 	LEFT_PAREN TokenType = iota // iota starts at 0 and increments for each constant
 	RIGHT_PAREN
 	LEFT_BRACE
 	RIGHT_BRACE
-	COMMMA
+	COMMA
 	DOT
 	MINUS
 	PLUS
@@ -60,3 +77,98 @@ const (
 
 	EOF
 )
+
+func (tt TokenType) String() string {
+	switch tt {
+	// Single-character tokens
+	case LEFT_PAREN:
+		return "("
+	case RIGHT_PAREN:
+		return ")"
+	case LEFT_BRACE:
+		return "{"
+	case RIGHT_BRACE:
+		return "}"
+	case COMMA:
+		return ","
+	case DOT:
+		return "."
+	case MINUS:
+		return "-"
+	case PLUS:
+		return "+"
+	case SEMICOLON:
+		return ";"
+	case SLASH:
+		return "/"
+	case STAR:
+		return "*"
+
+	// One or two character tokens
+	case BANG:
+		return "!"
+	case BANG_EQUAL:
+		return "!="
+	case EQUAL:
+		return "="
+	case EQUAL_EQUAL:
+		return "=="
+	case GREATER:
+		return ">"
+	case GREATER_EQUAL:
+		return ">="
+	case LESS:
+		return "<"
+	case LESS_EQUAL:
+		return "<="
+
+	// Literals
+	case IDENTIFIER:
+		return "IDENTIFIER"
+	case STRING:
+		return "STRING"
+	case NUMBER:
+		return "NUMBER"
+
+	// Keywords
+	case AND:
+		return "and"
+	case CLASS:
+		return "class"
+	case ELSE:
+		return "else"
+	case FALSE:
+		return "false"
+	case FUN:
+		return "fun"
+	case FOR:
+		return "for"
+	case IF:
+		return "if"
+	case NIL:
+		return "nil"
+	case OR:
+		return "or"
+	case PRINT:
+		return "print"
+	case RETURN:
+		return "return"
+	case SUPER:
+		return "super"
+	case THIS:
+		return "this"
+	case TRUE:
+		return "true"
+	case VAR:
+		return "var"
+	case WHILE:
+		return "while"
+
+	// End of file
+	case EOF:
+		return "EOF"
+
+	default:
+		return "UNKNOWN"
+	}
+}
